aa5d4feb80d1612f04099947ce417a3f9ea681a7
Praveen Choudhary <pc852180@gmail.com>
2025-10-15 15:45:54 +0530

    Brand Site

--- Client/src/Cancel.jsx
+++ Client/src/Cancel.jsx
@@ -1,5 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const Cancel = () => {
-  return <h1>Payment Canceled. Please try again.</h1>;
+   const navigate = useNavigate();
+    return(
+        <>
+          <h1>Payment Canceled. Please try again.</h1>
+   <button style={{textAlign:"center", marginLeft:"200px"}} onClick={()=>{navigate("/")}} >back to home</button>
+        </>
+    )
+  
+
 };
 
 export default Cancel;
